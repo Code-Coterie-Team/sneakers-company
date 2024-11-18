09030987cbb7927feb6aa2ff9ae9141e2d01f470
faeze <faezeh.am4212@gmail.com>
2024-11-18 19:56:37 +0330

    createHeader

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,25 @@ function App() {
 
   return (
 
-    <div className='flex justify-center'>
+    <div className='contain h-screen flex flex-col  items-center gap-24'>
       <Header />
+      <div className='main flex gap-7'>
+
+        <div className="imgProduct w-[425px] h-[430px] ">
+          <div className='imgFull'>
+            <img className='rounded-[20px]' src="src/image/image-product-1.jpg" alt="" />
+          </div>
+          <div className='imgList'>
+            <div>
+              <img src="" alt="" />
+            </div>
+          </div>
+        </div>
+        <div className='productDetail'>
+
+        </div>
+      </div>
+
     </div>
 
   )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,33 +2,28 @@
 function Header() {
 
     return (
-        <>
-            <div className="flex gap-80 pt-5 justify-between items-center">
+        
+            <div className="flex gap-80 pt-5 justify-between items-center border-solid border-b-2 border-gray-300 pb-8 px-7">
                 <div className="flex items-center justify-center gap-16">
                     <div>
                         <img src="src/image/logo.svg" alt="sneakers-text" />
                     </div>
                     <div>
                         <ul className="flex gap-10">
-                            <li className="text-dg-blue"><a href="#">Collections</a></li>
-                            <li className="text-dg-blue"><a href="#">Men</a></li>
-                            <li className="text-dg-blue"><a href="#">Women</a></li>
-                            <li className="text-dg-blue"><a href="#">About</a></li>
-                            <li className="text-dg-blue"><a href="#">Contact</a></li>
+                            <li className="text-dg-blue"><a className="border-b-4 border-white transition-all hover:transition-all delay-75 hover:border-b-4 hover:border-orange-500 hover:pb-[44px]" href="#">Collections</a></li>
+                            <li className="text-dg-blue"><a className="border-b-4 border-white transition-all  hover:transition-all delay-75 hover:border-b-4 hover:border-orange-500 hover:pb-[44px]" href="#">Men</a></li>
+                            <li className="text-dg-blue"><a className="border-b-4 border-white transition-all  hover:transition-all delay-75 hover:border-b-4 hover:border-orange-500 hover:pb-[44px]" href="#">Women</a></li>
+                            <li className="text-dg-blue"><a className="border-b-4 border-white transition-all  hover:transition-all delay-75 hover:border-b-4 hover:border-orange-500 hover:pb-[44px]" href="#">About</a></li>
+                            <li className="text-dg-blue"><a className="border-b-4 border-white transition-all  hover:transition-all delay-75 hover:border-b-4 hover:border-orange-500 hover:pb-[44px]" href="#">Contact</a></li>
                         </ul>
                     </div>
-
                 </div>
                 <div className="flex items-center justify-center gap-3">
-                    
-                        <img className="w-[40px] h-[40px] cursor-pointer text-gray-700" src="src/image/cart01.jpg" alt="cart" />
-                        <img className="w-[50px] h-[50px]" src="https://nasim67reja.github.io/Ecommerce.github.io/images/image-avatar.png" alt="profile" />
-                   
+                    <img className="w-[40px] h-[40px] cursor-pointer text-gray-700" src="src/image/cart01.jpg" alt="cart" />
+                    <img className="w-[50px] h-[50px]" src="https://nasim67reja.github.io/Ecommerce.github.io/images/image-avatar.png" alt="profile" />
                 </div>
-
-
             </div>
-        </>
+
     )
 }
 
