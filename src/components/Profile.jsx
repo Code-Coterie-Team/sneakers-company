0a860332fe377a5c0eaa32b306e252a8824deff4
faeze <faezeh.am4212@gmail.com>
2025-02-16 18:03:07 +0330

    createProfile

---
+++ src/components/Profile.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Profile() {
+  return (
+    <div className="profile w-[50px] h-[50px]">
+      <img
+        className="relative"
+        src="https://nasim67reja.github.io/Ecommerce.github.io/images/image-avatar.png"
+        alt="profile"
+      ></img>
+    </div>
+  );
+}
+
+export default Profile;
