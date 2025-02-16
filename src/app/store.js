3ec101bf115d0d795c79836b0a50661abbd1503f
faeze <faezeh.am4212@gmail.com>
2025-02-16 12:36:35 +0330

    createNumberRedux

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,10 @@
-import { configureStore } from '@reduxjs/toolkit'
-import themeSlice from '../features/themeSlice'
-
+import { configureStore } from "@reduxjs/toolkit";
+import themeSlice from "../features/themeSlice";
+import numberSlice from "../features/numberSlice";
 
 export default configureStore({
   reducer: {
     theme: themeSlice,
-   
+    number: numberSlice,
   },
-})
+});
